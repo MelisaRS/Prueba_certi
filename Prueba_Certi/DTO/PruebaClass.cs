8e951cf2dfef38fe57024c6feb89ca4ba6252b6a
Melisa Rojas <melisa.rs.1990@gmail.com>
2020-04-20 02:03:33 -0400

    Adding DTO

---
+++ Prueba_Certi/DTO/PruebaClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prueba_Certi.DTO
+{
+    public class PruebaClass
+    {
+        public string Name { get; set; }
+    }
+}
