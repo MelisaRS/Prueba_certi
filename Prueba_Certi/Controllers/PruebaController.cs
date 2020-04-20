fa4889e0daed09e75a10d7e24d76d039df432f92
Melisa Rojas <melisa.rs.1990@gmail.com>
2020-04-20 02:18:11 -0400

    Controllers

---
+++ Prueba_Certi/Controllers/PruebaController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prueba_Certi.Controllers
+{
+    public class PruebaController
+    {
+        /*
+         * Aqui estaran los controllers
+         */
+    }
+}
